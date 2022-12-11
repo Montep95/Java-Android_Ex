589e12f2e4db2e4ee2517d1e1df1b8991abcf51b
No BongJun <sewd1224@gmail.com>
2022-12-11 19:28:01 +0900

    [Add] <BitmapImage_Ex> Using Bitmap (Centrally placed image)

---
+++ BitmapImage_Ex/app/src/main/java/com/example/bitmapimage_ex/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.bitmapimage_ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyGraphicView(this));
+    }
+
+    private static class MyGraphicView extends View {
+
+
+        public MyGraphicView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.artist01_crop);
+
+            // 화면 중앙 위치값을 구하는 식
+            int picX = (this.getWidth() - picture.getWidth()) / 2;
+            int picY = (this.getHeight() - picture.getHeight()) / 2;
+
+            canvas.drawBitmap(picture, picX, picY, null);
+
+            // 비트맵 리소스를 해제
+            picture.recycle();
+        }
+    }
+}
